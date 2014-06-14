fcc6d11b2d9716d138600d8eb588ab47d79c46fb
Dmitry Skobelev <skobelev@informatics.by>
2014-06-15 00:08:39 +0300

    Searching commands added

---
+++ source/Seed.Dacs.MsSql.Components/MsSqlCommands/Quiz/SearchAvailableSingleQuestionsCommand.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Data;
+using System.Data.SqlClient;
+using Seed.Dacs.MsSql.Components.MsSqlHelpers;
+using Seed.Entities;
+
+namespace Seed.Dacs.MsSql.Components.MsSqlCommands.Quiz
+{
+    internal class SearchAvailableSingleQuestionsCommand
+    {
+    }
+}
